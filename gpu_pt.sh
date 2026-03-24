77ed6c49e8bd177a2e31fa1edbab29f903158578
juanjo44 <juanjosebetancourtosorio@gmail.com>
2026-03-24 18:51:32 -0500

    Primer commit con la version inicial, previo a correr

---
+++ gpu_pt.sh
@@ -0,0 +1,5 @@
+.\.venv\Scripts\activate
+pip uninstall torch torchvision torchaudio -y
+pip uninstall torch torchvision torchaudio -y
+pip cache purge
+pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
